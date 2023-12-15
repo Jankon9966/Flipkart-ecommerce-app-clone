84c412f568cde0708179d8102f92c205dd1ef2e7
Janko Novakovic <licardian9996@gmail.com>
2023-12-16 00:18:16 +0100

    initial commit

--- src/components/Header/Cart/CartContent.jsx
+++ src/components/Header/Cart/CartContent.jsx
@@ -2,12 +2,23 @@ import React, { useState } from "react";
 import { CartItem } from "./CartItem";
 
 const CartContent = () => {
+  
+  const [empty, setEmpty] = useState(true);
+
+  if (!empty) {
+    setEmpty(false);
+  }
+
   return (
     <div className="cart_content">
       <h1>Your Shopping Cart</h1>
       <div className="cart">
         <div className="cart_item_container">
-          <h2 className="empty">Your Flipkart Cart is empty</h2>
+          {empty ? (
+            <h2 className="empty">Your Flipkart Cart is empty</h2>
+          ) : (
+            <CartItem />
+          )}
         </div>
         <div className="cart_items_sub_total">
           <h3>Subtotal: $0</h3>
